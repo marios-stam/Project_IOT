548e2785e9cacd43de11fabe64a9eff83248d5fb
Frank Fourlas <frank.fourlas@gmail.com>
2021-11-25 03:13:07 +0200

    Fixed responses

--- Utils/dummy_sensors.py
+++ Utils/dummy_sensors.py
@@ -91,20 +91,33 @@ class AccelData:
 
 # =========== RESOURCE CLASSES ===========
 class Sensors(Resource):
-    def get(self, sensor_id):
+    def get(self, sensor_id=None):
         if sensor_id in gateway.get_sensor_IDs():
             return gateway.get_sensor_details(sensor_id), 200
+        elif sensor_id is None:
+            abort(400, message="No ID provided")
         else:
             abort(404, message="Sensor with ID {} doesn't exist".format(sensor_id))
 
-    def post(self, sensor_id):
-        pass
+    def post(self, sensor_id=None):
+        abort(501, message="Not implemented yet")
+
+    def put(self, sensor_id=None):
+        abort(501, message="Not implemented yet")
+
+    def delete(self, sensor_id=None):
+        if sensor_id in gateway.get_sensor_IDs():
+            abort(501, message="Not implemented yet")
+        elif sensor_id is None:
+            abort(400, message="No ID provided")
+        else:
+            abort(404, message="Sensor with ID {} doesn't exist".format(sensor_id))
 
 
 class Measurements(Resource):
     def get(self, sensor_id, count=1):
         if sensor_id in gateway.get_sensor_IDs():
-            return gateway.get_last_measurements(sensor_id, count)
+            return gateway.get_last_measurements(sensor_id, count), 200
         else:
             abort(404, message="Sensor with ID {} doesn't exist".format(sensor_id))
 
@@ -112,6 +125,11 @@ class Measurements(Resource):
 class AllSensors(Resource):
     def get(self):
         return gateway.get_sensor_IDs(), 200
+
+
+class Kill(Resource):
+    def get(self):
+        pass
 
 
 # =========== SENSOR CLASSES ===========
@@ -234,7 +252,7 @@ class SensorGateway:
 
 if __name__ == '__main__':
     # Routing RESTful API endpoints
-    api.add_resource(Sensors, '/sensor/<string:sensor_id>')
+    api.add_resource(Sensors, '/sensor/<string:sensor_id>', '/sensor')
     api.add_resource(Measurements, '/measurement/<string:sensor_id>', '/measurement/<string:sensor_id>/<int:count>')
     api.add_resource(AllSensors, '/sensor_list')
 
