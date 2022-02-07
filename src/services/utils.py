83752b2138e67741ff5942fbb5ec0a1bd128c3f6
Frank Fourlas <frank.fourlas@gmail.com>
2022-02-07 15:27:04 +0100

    Regressor

--- src/services/utils.py
+++ src/services/utils.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 from src.utils.utils import rand_perc, rand_check
 import numpy as np
+import json
 
 
 # === MOCK DATA ===
@@ -13,13 +14,31 @@ def rand_history_to_full():
     return np.array(res)
 
 
-def rand_history(hours=168):
-    base_rate = 2.5 + (1 * rand_perc(center=True))
+def rand_history(id, hours=168):
+    # Try to get data from file
+    try:
+        with open('./bin/dummy_rates.json', 'r', encoding='utf-8') as f:
+            base_rates = json.load(f)
+    # Create file if missing
+    except FileNotFoundError:
+        with open('./bin/dummy_rates.json', 'w', encoding='utf-8') as f:
+            json.dump({}, f, indent=4)
+
+        return rand_history(id, hours)
+
+    # Generate base rate for new IDs
+    if id not in base_rates:
+        base_rates[id] = 2.5 + (1 * rand_perc(center=True))
+
+        with open('./bin/dummy_rates.json', 'w', encoding='utf-8') as f:
+            json.dump(base_rates, f, indent=4)
+
     res = [0]
 
+    # Generate data
     for i in range(hours):
-        res.append(min(100, res[-1] + base_rate * max(0, (0.5 + 0.75 * rand_perc(center=True)))))
-        if rand_check(rand_perc() * 0.05 * res[-1] / 100 ):
+        res.append(min(100, res[-1] + base_rates[id] * max(0, (0.5 + 0.75 * rand_perc(center=True)))))
+        if rand_check(rand_perc() * 0.05 * res[-1] / 100):
             res[-1] = 0
 
     return np.array(res)
@@ -56,14 +75,14 @@ def graph_rand_to_full_history(rep=100, sensor_id=None):
     plt.show()
 
 
-def graph_rand_history(rep=9, sensor_id=None):
+def graph_rand_history(rep=10):
     tmp = None
 
     f = plt.figure(figsize=(8, 4))
-    ax0 = f.add_subplot('111')
+    ax0 = f.add_subplot(111)
 
     for i in range(rep):
-        tmp = rand_history()
+        tmp = rand_history(id=str(i))
         ax0.plot(range(len(tmp)), tmp, label="sim " + str(i+1))
 
     ax0.set_xlim(0, len(tmp))
