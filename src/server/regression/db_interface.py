12e51bc22474c0a0ffc65d2514d014de4aeb2878
Frank Fourlas <frank.fourlas@gmail.com>
2022-02-09 22:24:40 +0100

    Regression Fully Tested

--- src/server/regression/db_interface.py
+++ src/server/regression/db_interface.py
@@ -12,7 +12,7 @@ def get_angle(sensor_id):
         Regression.sensor_id == sensor_id).all()
 
     if len(result) > 0:
-        print(f"Gettind data of bin: {sensor_id}...")
+        print(f"Entry for {sensor_id} found")
         data = result[0].__dict__
 
         t1 = datetime.strptime(data['timestamp'][:-4], '%Y-%m-%d %H:%M:%S')
@@ -24,6 +24,7 @@ def get_angle(sensor_id):
             'timestamp': datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S.000')
         }
         if len(result) <= 0:
+            print(f"No entry was found for {sensor_id}... calculating")
             # Create new DB entry for the sensor with the above data
             new_regression = Regression(**data)
 
@@ -31,10 +32,15 @@ def get_angle(sensor_id):
             db.session.add(new_regression)
             db.session.commit()
         else:
+            print(f"Entry was old... re-calculating")
             # Update existon entry with the above angle
             result[0].angle = data['angle']
             result[0].timestamp = data['timestamp']
             db.session.commit()
 
-    data.pop('_sa_instance_state')
+    # print(data)
+    try:
+        data.pop('_sa_instance_state')
+    except KeyError:
+        pass
     return make_response(jsonify(data))
