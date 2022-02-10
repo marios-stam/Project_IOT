80f06ca2b3669d552f31ba69acc1b06893a4f54e
Frank Fourlas <frank.fourlas@gmail.com>
2022-02-10 01:19:42 +0100

    Path fix

--- src/server/trucks/truck_routing/trav_salesman_problem/calculate.py
+++ src/server/trucks/truck_routing/trav_salesman_problem/calculate.py
@@ -10,10 +10,15 @@ except:
 
 
 # print("Current working directory:", os.getcwd())
-problem_file_name = r"src\server\trucks\truck_routing\trav_salesman_problem\problem.json"
-result_file_name = r"src\server\trucks\truck_routing\trav_salesman_problem\result.json"
-final_result_file_name = r"src\server\trucks\truck_routing\trav_salesman_problem\final_result.json"
-api_key_file_name = r"src\server\trucks\truck_routing\trav_salesman_problem\api_key.txt"
+# problem_file_name = r"src\server\trucks\truck_routing\trav_salesman_problem\problem.json"
+# result_file_name = r"src\server\trucks\truck_routing\trav_salesman_problem\result.json"
+# final_result_file_name = r"src\server\trucks\truck_routing\trav_salesman_problem\final_result.json"
+# api_key_file_name = r"src\server\trucks\truck_routing\trav_salesman_problem\api_key.txt"
+
+problem_file_name = os.path.join(os.getcwd(), "server/trucks/truck_routing/trav_salesman_problem/problem.json")
+result_file_name = os.path.join(os.getcwd(), "server/trucks/truck_routing/trav_salesman_problem/result.json")
+final_result_file_name = os.path.join(os.getcwd(), "server/trucks/truck_routing/trav_salesman_problem/final_result.json")
+api_key_file_name = os.path.join(os.getcwd(), "server/trucks/truck_routing/trav_salesman_problem/api_key.txt")
 
 
 def load_key():  # load key from .txt file
